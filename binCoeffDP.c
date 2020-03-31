2ced1d9919ea5c205caedcae7c72b6ed730fd74d
Harish Sanjaykumar Pukale <46403164+harishsanjaykumarpukale@users.noreply.github.com>
2020-03-31 23:52:22 +0530

    Create binCoeffDP.c

---
+++ binCoeffDP.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int binCoeff(int n,int r){
+    int arr[n+1][r+1],i,j;
+    for(i=1;i<=n;i++)
+        for(j=1;j<=i;j++){
+            if(i==j)
+                arr[i][j]=1;            
+            else if(j==1)
+                arr[i][j]=i;
+            else
+                arr[i][j]=arr[i-1][j] + arr[i-1][j-1];
+        }
+    
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++)
+            printf("\t%d",arr[i][j]);
+        printf("\n");
+    }
+
+    return arr[n][r];               
+    
+}
+
+int main(){
+    printf("%d",binCoeff(5,3));
+}
+
+// Time complexity
+// As this algo calculates almost half  
+// of matrix nxr it takes time O(n^2)
